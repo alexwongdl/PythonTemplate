03cb310737c0fa0273913cd7ba7840c8818ddd56
AlexWang90 <wangjian90@hotmail.com>
2017-06-16 21:02:19 +0800

    MOD：mini modification

--- test/test_function.py
+++ test/test_function.py
@@ -17,9 +17,14 @@ def test_none():
     if devide(2,0) is None:
         print("devide(2,0) is None")
 
-
+def arg_num_wunknow(*args, **kwargs):
+    for arg in args:
+        print("arg:" + str(arg))
+    for key in kwargs:
+        print("kwargs:" + key + "\t" + kwargs[key])
 
 if __name__ == "__main__":
     test_none()
     if 0 is None:
         print("0 is None")
+    arg_num_wunknow(45,23,"abc", karg_one="one", karg_two= "two")
