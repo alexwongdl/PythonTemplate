91b494b4d39a31592ddfced391a9a46885d30f25
AlexWang90 <wangjian90@hotmail.com>
2017-06-23 17:18:04 +0800

    ADD：测试tf.nn.embedding_lookup

---
+++ test/tftest/embedding_test.py
@@ -0,0 +1,22 @@
+"""
+Created by Alex Wang on 2017-06-23
+测试tf.nn.embedding_lookup
+"""
+import tensorflow as tf
+import numpy as np
+
+def test_embedding_lookup():
+    """
+    测试tf.nn.embedding_lookup
+    :return:
+    """
+    matrix = tf.Variable(tf.random_uniform([100, 20],0,1 ), name="dict")
+    ids = np.array([1,4,5])
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    x = tf.nn.embedding_lookup(matrix, ids)
+    x_value = sess.run([x])
+    print(x_value)
+
+if __name__ == "__main__":
+    test_embedding_lookup()
