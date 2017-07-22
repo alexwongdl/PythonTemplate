578f0871e8e09c06ff18491e66f6253e457787fa
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-07-22 18:54:00 +0800

    MOD：mini modification

--- test/tftemplate/tornado/BasicServer.py
+++ test/tftemplate/tornado/BasicServer.py
@@ -9,17 +9,20 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 from tornado.options import define
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 define("port", default=18015, help="run on the given port", type=int)
 
 import tensorflow as tf
 a = tf.placeholder(tf.int32, shape=(), name="input")
 asquare = tf.multiply(a, a, name="output")
-sess = tf.Session()
+config = tf.ConfigProto(device_count={"CPU": 20}, inter_op_parallelism_threads=10, intra_op_parallelism_threads=10)
+sess = tf.Session(config=config)
 
 class IndexHandler(tornado.web.RequestHandler):
-    def initialize(self):
-        self.sess = sess
+    def initialize(self, thesess):
+        self.sess = thesess
 
     def get(self):
         num = self.get_argument('num', 2)
@@ -29,11 +32,11 @@ class IndexHandler(tornado.web.RequestHandler):
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler,dict(thesess=sess))])
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(18824)
     tornado.ioloop.IOLoop.current().start()
 
     # http_server.bind(18825)
-    # http_server.start(0)
+    # http_server.start(10)
     # tornado.ioloop.IOLoop.current().start()
--- test/tftemplate/tornado/BasicServerAsyn.py
+++ test/tftemplate/tornado/BasicServerAsyn.py
@@ -4,23 +4,27 @@ on 20170721
 
 Tornado + Tensorflow 多线程
 """
+import os
+from concurrent.futures import ThreadPoolExecutor
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-from tornado.options import define
-from concurrent.futures import ThreadPoolExecutor
 from tornado.concurrent import run_on_executor
-import time
-import os
+from tornado.options import define
 
-os.environ["CUDA_VISIBLE_DEVICES"]=""
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 define("port", default=18015, help="run on the given port", type=int)
 
 import tensorflow as tf
-a = tf.placeholder(tf.int32, shape=(), name="input")
+import numpy as np
+
+a = tf.placeholder(tf.float32, shape=(100, 100), name="input")
 asquare = tf.multiply(a, a, name="output")
-sess = tf.Session()
+config = tf.ConfigProto(device_count={"CPU": 20}, inter_op_parallelism_threads=10, intra_op_parallelism_threads=10)
+sess = tf.Session(config=config)
+
 
 class IndexHandler(tornado.web.RequestHandler):
     executor = ThreadPoolExecutor(4)
@@ -38,9 +42,10 @@ class IndexHandler(tornado.web.RequestHandler):
     # 线程里处理
     @run_on_executor
     def my_func(self, num):
-        for i in range(10000):
-            data = self.sess.run([asquare], feed_dict={a: num})
+        arr = np.random.random((100,100))
+        data = self.sess.run([asquare], feed_dict={a: arr})
         return data
+
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
@@ -49,6 +54,6 @@ if __name__ == "__main__":
     http_server.listen(18844)
     tornado.ioloop.IOLoop.current().start()
 
-    #http_server.bind(18825)
-    #http_server.start(3)
-    #tornado.ioloop.IOLoop.current().start()
+    # http_server.bind(18825)
+    # http_server.start(3)
+    # tornado.ioloop.IOLoop.current().start()
