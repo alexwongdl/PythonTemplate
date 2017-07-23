c1be461b6d544bcf2b1c775ff420887db1d6e36c
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-07-23 19:24:48 +0800

    ADD：tornado多进程

---
+++ test/tftemplate/tornado/BasicServerMultiProcess.py
@@ -0,0 +1,48 @@
+"""
+Created by Alex.Wang
+on 20170721
+
+Tornado + Tensorflow 单线程
+"""
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado.options import define
+import os
+import tensorflow as tf
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+define("port", default=18015, help="run on the given port", type=int)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        num = self.get_argument('num', 2)
+        for i in range (1000):
+            ret = self.application.calltf(num)
+        self.write( 'result:' + str(ret))
+
+class TFApplication(tornado.web.Application):
+    def __init__(self):
+        handlers=[(r"/", IndexHandler)]
+        super(TFApplication, self).__init__(handlers)
+
+        ## 初始化tensorflow
+        self.a = tf.placeholder(tf.int32, shape=(), name="input")
+        self.asquare = tf.multiply(self.a, self.a, name="output")
+        config = tf.ConfigProto(device_count={"CPU": 20}, inter_op_parallelism_threads=10, intra_op_parallelism_threads=10)
+        self.sess = tf.Session(config=config)
+        print(self.sess.run([self.asquare], feed_dict={self.a: 32}))
+
+    def calltf(self, num):
+        print("num is:" + str(num))
+        return self.sess.run([self.asquare], feed_dict={self.a: num})
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(TFApplication())
+
+    http_server.bind(18825)
+    http_server.start(2)
+    tornado.ioloop.IOLoop.current().start()
