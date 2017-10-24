642085c0498cb0b42aa9f53866121da220f7a5a6
Alex.W <wangjian90@hotmail.com>
2017-10-24 23:21:11 +0800

    ADD：添加print模块

--- test/test_argparse.py
+++ test/test_argparse.py
@@ -3,6 +3,7 @@ Created by Alex Wang
 On 2017-10-15
 """
 import argparse
+from myutil.myprint import *
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -10,5 +11,6 @@ if __name__ == "__main__":
     parser.add_argument('-n', '--times', default='1', help='run times', type=int)
 
     args = parser.parse_args()
-    print(args.type)
-    print("times:" + str(args.times + 5))
+    # print(args.type)
+    # print("times:" + str(args.times + 5))
+    arg_parse_print(args)
