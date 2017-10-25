741c45e5ca6c6d8847a3965084569d9167a47bc0
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-10-25 14:38:32 +0800

    MOD：argparser测试

--- test/test_argparse.py
+++ test/test_argparse.py
@@ -3,15 +3,17 @@ Created by Alex Wang
 On 2017-10-15
 """
 import argparse
+
 from myutil.myprint import *
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--type', default='mehod_one', help='method type', type= str)
+    parser.add_argument('-t', '--type', default='mehod_one', help='method type', type=str)
     parser.add_argument('-n', '--times', default='1', help='run times', type=int)
+    parser.add_argument('--name', default=None, help='your name', type=str)
 
     args = parser.parse_args()
     # print(args.type)
     # print("times:" + str(args.times + 5))
-    
+
     arg_parse_print(args)
