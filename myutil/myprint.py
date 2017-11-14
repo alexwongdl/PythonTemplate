9e35a54a20d2eba278911b3202a9aacd29075802
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-11-14 10:52:54 +0800

    MOD：myprint.py失效，功能合并到printutil.py

--- myutil/myprint.py
+++ myutil/myprint.py
@@ -3,12 +3,16 @@
 Created by Alex Wang
 On 2017-10-24
 """
+import warnings
+import argparse
+
 def arg_parse_print(FLAGS):
     """
     FLAGS = parser.parse_args()
     :param FLAGS:
     :return:
     """
+    warnings.warn("myprint.py is deprecated, please use printutil instead.")
     print('[Configurations]:')
     for name in FLAGS.__dict__.keys():
         value = FLAGS.__dict__[name]
@@ -24,3 +28,10 @@ def arg_parse_print(FLAGS):
             print('\t%s: %s' % (name, value))
 
     print('[End of configuration]')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--xx', default='x')
+    parser.add_argument('--oo', default='o')
+    FLAGS = parser.parse_args()
+    arg_parse_print(FLAGS)
--- myutil/printutil.py
+++ myutil/printutil.py
@@ -78,6 +78,28 @@ def test():
     print('*' * 50)
     print('\033[0m')
 
+def arg_parse_print(FLAGS):
+    """
+    FLAGS = parser.parse_args()
+    :param FLAGS:
+    :return:
+    """
+    print('[Configurations]:')
+    for name in FLAGS.__dict__.keys():
+        value = FLAGS.__dict__[name]
+        if type(value) == float:
+            print('\t%s: %f'%(name, value))
+        elif type(value) == int:
+            print('\t%s: %d'%(name, value))
+        elif type(value) == str:
+            print('\t%s: %s'%(name, value))
+        elif type(value) == bool:
+            print('\t%s: %s'%(name, value))
+        else:
+            print('\t%s: %s' % (name, value))
+
+    print('[End of configuration]')
+
 if __name__ == "__main__":
     print("__main__")
     # test()
