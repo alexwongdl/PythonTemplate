ba79b8d7f5e6dedf725d007e73050fda466b3b28
Alex.W <wangjian90@hotmail.com>
2018-01-28 23:45:26 +0800

    MOD：opencv GUI test

---
+++ test/test_opencv/img_display.py
@@ -0,0 +1,14 @@
+import cv2
+
+def test_imshow():
+    """
+    测试opencv图像展示
+    :return:
+    """
+    img = cv2.imread('E://blog/oxford_pet.png')
+    cv2.imshow('window1', img)
+    k = cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    test_imshow()
