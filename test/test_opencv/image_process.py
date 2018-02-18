fc2f911912c492779033fc39c4c960686a4c05b9
Alex.W <wangjian90@hotmail.com>
2018-02-18 21:18:06 +0800

    MOD:image smooth and edge detect

--- test/test_opencv/image_process.py
+++ test/test_opencv/image_process.py
@@ -3,6 +3,7 @@ Created by Alex Wang on 2018-02-03
 """
 
 import cv2
+import numpy as np
 
 def color_space():
     """
@@ -12,5 +13,111 @@ def color_space():
     flags = [i for i in dir(cv2) if i.startswith('COLOR_BGR')]
     [print(flag) for flag in flags]
 
+def image_resize():
+    """
+    图像缩放
+    :return:
+    """
+    img = cv2.imread('dl.jpg')
+
+    # img_resize_factor = cv2.resize(img, None, fx=2, fy=2, interpolation = cv2.INTER_AREA)
+    img_resize_factor = cv2.resize(img, None, fx=2, fy=2, interpolation = cv2.INTER_LINEAR)
+    cv2.imshow('dl_refactor',img_resize_factor)
+
+    rows, cols = img.shape[:2]
+    img_resize = cv2.resize(img, (int(0.5 * cols), int(0.5 * rows)), interpolation = cv2.INTER_CUBIC)
+    cv2.imshow('dl_resize', img_resize)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def test_transformation():
+    """
+    图像平移、旋转、仿射变换
+    :return:
+    """
+    img = cv2.imread('dl.jpg')
+    rows, cols = img.shape[:2]
+    # 平移
+    shift_mat = np.array([[1,0,20],[0,1,50]], dtype=np.float32) # x方向平移20像素，y方向移动50像素
+    img_shift = cv2.warpAffine(img, shift_mat, (cols + 20, rows + 50))  #最后一个参数是输出图像大小
+    cv2.imshow('img_shift', img_shift)
+
+    # 旋转
+    rotate_mat = cv2.getRotationMatrix2D((cols / 2, rows / 2), 10, 1)
+    # rotate_mat = cv2.getRotationMatrix2D((0, 0), 10, 1)
+    print(rotate_mat)
+    img_rotate =  cv2.warpAffine(img, rotate_mat, (cols + 30, rows + 30))
+    cv2.imshow('img_rotate', img_rotate)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def test_kernel_conv():
+    """
+    图像卷积：模糊、
+    :return:
+    """
+    img = cv2.imread('dl.jpg')
+    avg_kernel = np.ones((5,5), dtype=np.float32) / 25
+    # 应用kernel
+    img_avg = cv2.filter2D(img, -1, avg_kernel)
+    # 均值滤波
+    img_blur = cv2.blur(img, (5,5))
+    # 高斯滤波
+    gaussian_kernel = cv2.getGaussianKernel(3, 1)
+    print('gaussian_kernel:', gaussian_kernel)
+    gaussian_img = cv2.GaussianBlur(img, (3,3), sigmaX=1, sigmaY = 2)
+    # 中值滤波
+    median_img = cv2.medianBlur(img, 5)
+    #双边滤波
+    bilateral_img = cv2.bilateralFilter(img, 9, 75, 75)
+
+    cv2.imshow('org_img', img)
+    cv2.imshow('img_avg', img_avg)
+    cv2.imshow('img_blur', img_blur)
+    cv2.imshow('img_gaussian', gaussian_img)
+    cv2.imshow('img_median', median_img)
+    cv2.imshow('img_bilateral', bilateral_img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def test_edge_detect():
+    """
+    图像边缘检测：Sobel、Laplacian、Canny
+    :return:
+    """
+    img = cv2.imread('dl.jpg')
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Sobel
+    gaussian_img = cv2.GaussianBlur(img_gray, (3,3), sigmaX=1, sigmaY = 1)
+    img_sobelx = cv2.Sobel(gaussian_img, cv2.CV_8U, dx = 1, dy = 0, ksize = 3)
+    img_sobely = cv2.Sobel(gaussian_img, cv2.CV_8U, dx = 0, dy = 1, ksize = 3)
+    img_sobel_weight = cv2.addWeighted(img_sobelx, 0.5, img_sobely, 0.5, 0)
+    img_sobelxy = cv2.Sobel(gaussian_img, cv2.CV_8U, dx = 1, dy = 1, ksize = 3)
+
+    # Laplacian
+    img_laplacian = cv2.Laplacian(gaussian_img, cv2.CV_64F)
+
+    # Canny
+    img_canny = cv2.Canny(img, 50, 150, L2gradient = True)
+
+    cv2.imshow('img_gray', img_gray)
+    cv2.imshow('img_sobelx', img_sobelx)
+    cv2.imshow('img_sobely', img_sobely)
+    cv2.imshow('img_sobelxy', img_sobel_weight)
+    cv2.imshow('img_laplacian', img_laplacian)
+    cv2.imshow('img_canny', img_canny)
+    # cv2.imshow('img_sobelxy_1', img_sobelxy)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
     color_space()
+    # image_resize()
+    # test_transformation()
+    # test_kernel_conv()
+    test_edge_detect()
