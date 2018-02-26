af0415778b451a2b521cfff4fa925bc40c0fc5a8
Alex.W <wangjian90@hotmail.com>
2018-02-26 20:29:41 +0800

    MOD：test surf

--- test/test_opencv/image_feature.py
+++ test/test_opencv/image_feature.py
@@ -3,6 +3,7 @@ Created by Alex Wang on 2018-02-23
 """
 import cv2
 
+
 def test_harris():
     """
     Harris角点检测
@@ -12,13 +13,14 @@ def test_harris():
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     harris_corners = cv2.cornerHarris(img_gray, 2, 3, 0.04)
     harris_corners = cv2.dilate(harris_corners, None)
-    img[harris_corners > harris_corners.max() * 0.01] = [0,0,225]
+    img[harris_corners > harris_corners.max() * 0.01] = [0, 0, 225]
     print(harris_corners.max())
     print(harris_corners)
 
     cv2.imshow('img', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 def test_sift():
     """
@@ -43,6 +45,32 @@ def test_sift():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
+def test_surf():
+    """
+    SURF特征提取
+    :return:
+    """
+    img = cv2.imread('dl.jpg')
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    surf = cv2.xfeatures2d.SURF_create()
+    surf.setHessianThreshold(400)
+    kp, des = surf.detectAndCompute(img_gray, None)
+
+    print(dir(surf))
+    print('hessianThreshold:', surf.getHessianThreshold())
+    print('upright:', surf.getUpright())  # Upright-SURF
+    print('extended:', surf.getExtended())  # false==64d, true== 128d
+    print('descriptor num:', len(des))
+
+    img_kp = img.copy()
+    cv2.drawKeypoints(img, kp, img_kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imshow('img_kp', img_kp)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
     # test_harris()
-    test_sift()
+    # test_sift()
+    test_surf()
