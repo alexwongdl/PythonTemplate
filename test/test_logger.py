1573ae41df4b76105299981d856334eefa18356f
hzwangjian1 <hzwangjian1@corp.netease.com>
2018-02-27 18:32:29 +0800

    ADD：test logging fileConfig

---
+++ test/test_logger.py
@@ -0,0 +1,16 @@
+"""
+Created by Alex Wang on 2018-2-27
+测试logger.config
+"""
+
+import logging
+import logging.config
+
+logging.config.fileConfig('../resource/logger_config')
+logger = logging.getLogger('alexwang')
+
+def test_logging_conf():
+    logger.info('test logging fileConfig')
+
+if __name__ == '__main__':
+    test_logging_conf()
